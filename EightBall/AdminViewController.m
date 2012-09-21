41f8675c791ad6909795dad5e80ed32fd0124c1a
Codemis <johnathan@missionaldigerati.com>
2012-09-20 20:43:28 -0700

    Added delete in admin without persistence.

--- EightBall/AdminViewController.m
+++ EightBall/AdminViewController.m
@@ -10,7 +10,7 @@
 #import "AppDelegate.h"
 
 @interface AdminViewController ()
-@property (strong, nonatomic) NSArray *answersArray;
+@property (strong, nonatomic) NSMutableArray *answersArray;
 @end
 
 @implementation AdminViewController
@@ -19,7 +19,7 @@
 {
     AppDelegate *appDelegate = [UIApplication sharedApplication].delegate;
     NSSortDescriptor* sortOrder = [NSSortDescriptor sortDescriptorWithKey: @"self" ascending: YES];
-    self.answersArray = [appDelegate.answersArray sortedArrayUsingDescriptors: [NSArray arrayWithObject: sortOrder]];
+    self.answersArray = [NSMutableArray arrayWithArray:[appDelegate.answersArray sortedArrayUsingDescriptors: [NSArray arrayWithObject: sortOrder]]];
 }
 
 #pragma mark - Table view data source
@@ -39,44 +39,20 @@
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
 
-/*
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         // Delete the row from the data source
+        [self.answersArray removeObjectAtIndex:indexPath.row];
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
     }   
     else if (editingStyle == UITableViewCellEditingStyleInsert) {
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }   
 }
-*/
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 #pragma mark - Table view delegate
 
