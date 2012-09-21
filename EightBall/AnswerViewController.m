02c3c5c065cd33377eee882af7f1eec7d422b489
Codemis <johnathan@missionaldigerati.com>
2012-09-20 19:47:24 -0700

    Moved answers array into the AppDelegate.

--- EightBall/AnswerViewController.m
+++ EightBall/AnswerViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "AnswerViewController.h"
+#import "AppDelegate.h"
 
 @interface AnswerViewController ()
 @property (strong, nonatomic) IBOutlet UILabel *answerLabel;
@@ -17,26 +18,8 @@
 
 -(void)setAnswerText
 {
-    NSArray *answers = @[   @"It is certain.",
-    @"It is decidedly so.",
-    @"Without a doubt.",
-    @"Yes – definitely.",
-    @"You may rely on it.",
-    @"As I see it, yes.",
-    @"Most likely.",
-    @"Outlook good.",
-    @"Yes.",
-    @"Signs point to yes.",
-    @"Reply hazy, try again.",
-    @"Ask again later.",
-    @"Better not tell you now.",
-    @"Cannot predict now.",
-    @"Concentrate and ask again.",
-    @"Don't count on it.",
-    @"My reply is no.",
-    @"My sources say no.",
-    @"Outlook not so good.",
-    @"Very doubtful"];
+    AppDelegate *appDelegate = [UIApplication sharedApplication].delegate;
+    NSArray *answers = appDelegate.answersArray;
     int rndIndex = arc4random()%[answers count];
     self.answerLabel.text = answers[rndIndex];
 }
