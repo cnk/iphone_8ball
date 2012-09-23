b0ee54183a0ce707364bc0c8116edeba11fdb78e
Bruce Hobbs <brucehobbs@engineeredsw.com>
2012-09-22 17:59:42 -0700

    Simplify creation of answersArray

--- EightBall/AppDelegate.m
+++ EightBall/AppDelegate.m
@@ -1,39 +1,29 @@
-//
-//  AppDelegate.m
-//  EightBall
-//
-//  Created by Johnathan Pulos on 9/6/12.
-//  Copyright (c) 2012 Johnathan Pulos. All rights reserved.
-//
-
 #import "AppDelegate.h"
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-    // Override point for customization after application launch.
-    NSArray *defaultAnswersArray = @[@"It is certain.",
-    @"It is decidedly so.",
-    @"Without a doubt.",
-    @"Yes – definitely.",
-    @"You may rely on it.",
-    @"As I see it, yes.",
-    @"Most likely.",
-    @"Outlook good.",
-    @"Yes.",
-    @"Signs point to yes.",
-    @"Reply hazy, try again.",
-    @"Ask again later.",
-    @"Better not tell you now.",
-    @"Cannot predict now.",
-    @"Concentrate and ask again.",
-    @"Don't count on it.",
-    @"My reply is no.",
-    @"My sources say no.",
-    @"Outlook not so good.",
-    @"Very doubtful"];
-    self.answersArray = [NSMutableArray arrayWithArray:defaultAnswersArray];
+-(BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.answersArray = [@[@"It is certain.",
+                         @"It is decidedly so.",
+                         @"Without a doubt.",
+                         @"Yes – definitely.",
+                         @"You may rely on it.",
+                         @"As I see it, yes.",
+                         @"Most likely.",
+                         @"Outlook good.",
+                         @"Yes.",
+                         @"Signs point to yes.",
+                         @"Reply hazy, try again.",
+                         @"Ask again later.",
+                         @"Better not tell you now.",
+                         @"Cannot predict now.",
+                         @"Concentrate and ask again.",
+                         @"Don't count on it.",
+                         @"My reply is no.",
+                         @"My sources say no.",
+                         @"Outlook not so good.",
+                         @"Very doubtful"] mutableCopy];
     return YES;
 }
 							
