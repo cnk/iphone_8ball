16a6c7c381db055cbe1dfa656fba31614c4ce052
Codemis <johnathan@missionaldigerati.com>
2012-09-27 21:24:47 -0700

    Became delegate and return key closes keyboard.

--- EightBall/AnswerFormViewController.m
+++ EightBall/AnswerFormViewController.m
@@ -1,17 +1,21 @@
 #import "AnswerFormViewController.h"
 #import "AppDelegate.h"
 
-@interface AnswerFormViewController ()
+@interface AnswerFormViewController () <UITextFieldDelegate>
 @property (strong, nonatomic) IBOutlet UITextField *answerTextField;
 @property (strong, nonatomic) AppDelegate *appDelegate;
 @end
 
 @implementation AnswerFormViewController
-- (void)viewDidLoad
-{
+- (void)viewDidLoad{
     [super viewDidLoad];
     self.appDelegate = [UIApplication sharedApplication].delegate;
     self.answerTextField.text = [self.appDelegate answerAtIndex:self.answerIndex];
     [self.answerTextField becomeFirstResponder];
+}
+#pragma UITextFieldDelegate Methods
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    return YES;
 }
 @end
